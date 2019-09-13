afbc83a8a2432712c5b868a3fb7f7273edc747df
Swaminathan Nagarajan <swami701search@gmail.com>
2019-09-13 12:31:44 +0800

    DEPLOYMENT: Add deployment task

--- scripts/hc-node.js
+++ scripts/hc-node.js
@@ -1,3 +1,5 @@
+let cmd = require('node-cmd')
+let child = require('child_process');
 module.exports = (robot) => {
     robot.respond(/deploy (.*) (.*) (.*)/i, (res) => {
         let repo = res.match[1]
@@ -17,9 +19,35 @@ module.exports = (robot) => {
             return
         }
 
-        res.send(`Deployment Started! for *${repo}* for branch: \`${branch}\` in env: \`${env}\` :smile :+1:`)
-        setTimeout(() => {
-            res.send("Node Deploy! done!")
-        }, 1000);
+        res.send(`Deployment Started! for \`${repo}\` for branch: \`${branch}\` in env: \`${env}\` :smile :+1:`)
+
+        if (repo === 'node-hello-world') {
+            cmd.get(
+                `
+                cd /home/swami/Documents/deploy/node-hello-world
+                pwd
+                git pull
+                git checkout ${branch}
+                npm i
+                docker build -t hwn .;
+                `,
+                function(err, data, stderr){
+                    res.send(`\`\`\`${data}\`\`\``)
+                    child.exec('docker stop hwn; docker rm hwn; docker run -itd -p 3000:3000 --name=hwn hwn', (err, stdout, stderr) => {
+                        if (err) {
+                          //some err occurred
+                          res.reply(`\`\`\`${err}\`\`\``)
+                          res.reply("Failed deployment! :disappointed:")
+                        } else {
+                         // the *entire* stdout and stderr (buffered)
+                         console.log(`stdout: ${stdout}`);
+                         console.log(`stderr: ${stderr}`);
+                         res.send(`\`\`\`${stdout}\`\`\``)
+                        }
+                        res.reply(`Deployment Done! :+1:`)
+                    });
+                }
+            );
+        }
     })
 } 
